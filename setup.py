c168d3858373b944a30c739c028958bffe007b25
Tomizap <88057971+Tomizap@users.noreply.github.com>
2023-05-23 15:33:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['selenium', 'undetected_chromedriver'],
-    dependency_link=["git+git://github.com/ultrafunkamsterdam/undetected-chromedriver#undetected_chromedriver"],
+    install_requires=['selenium', 'undetected_chromedriver @ git+https://github.com/ultrafunkamsterdam/undetected-chromedriver#undetected_chromedriver'],
+    # dependency_link=["git+git://github.com/ultrafunkamsterdam/undetected-chromedriver#undetected_chromedriver"],
     keywords=[],
     classifiers=[]
 )
